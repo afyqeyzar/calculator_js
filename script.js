000b4920105132ff8cbda011843968ce332f89c2
afyqeyzar <norafyqe@gmail.com>
2022-08-02 18:11:45 +0100

    added id to buttons

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ function divide(num1, num2){
 function operate(operator, num1, num2){
     return operator(num1, num2)
 }
+
+const displayBox = document.querySelector('#display')
+var displayValue = 0
+
+function display(number){
+    displayBox.textContent = number
+}
+
+display(59)
